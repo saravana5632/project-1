5019040f98603c8d2079f09da470ebf73df6ab97
SaravanaKumar G <oatktg.saravana5632@gmail.com>
2025-08-15 11:48:47 +0530

    app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
        'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
        'Sharjah','Mohali', 'Bengaluru']
 
-pipe=pickle.load(open('pipe.pkl','rb'))
+pipe=pickle.load(open('project-1/pipe.pkl','rb'))
 
 st.title('IPL Prediction')
 
@@ -68,6 +68,7 @@ if st.button('Predict Probability'):
     st.write(bowling_team + "=" + str(round(loss*100)) + "%")
 
     
+
 
 
 
